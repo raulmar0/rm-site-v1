72769bdd5aa3797c47b6b407c99d6e0230dfbc60
raulmar <raulhdz40@gmail.com>
2022-07-21 20:36:09 -0500

    blog-gallery-v1 terminado

--- components/BlogGallery.tsx
+++ components/BlogGallery.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Link from "next/link";
 
 import { format } from "date-fns";
+import readingTime from "reading-time";
 
 // import { Pagination, IPaginationProps } from '../pagination/Pagination';
 // import { PostItems } from '../utils/Content';
@@ -12,30 +13,61 @@ import { format } from "date-fns";
 //   pagination: IPaginationProps;
 // };
 
-const BlogGallery = () => (
-  <>
+
+
+const BlogGallery = ({ posts }) => {
+  
+  const titles = posts.map(post => post.title);
+  const rawReadingTimes = posts.map(post => readingTime(post.html));
+  const readingTimes = rawReadingTimes.map(readingTime => readingTime.text);
+  const slugs = posts.map(post => post.slug);
+  const tags = posts.map(post => post.tags[0].name);
+  const rawDates = posts.map(post => post.published_at);
+  const dates = rawDates.map(date => format(new Date(date), 'dd/MM/yyyy'));
+
+  return (
+  <>    
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="px-5 mx-auto">
         <div className="-my-8 divide-y-2 divide-gray-100">
 
+        {
+          posts.map((post) => (
+          // eslint-disable-next-line react/jsx-key
           <div className="py-8 flex flex-wrap md:flex-nowrap">
             <div className="md:flex-grow">
-              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                Bitters hashtag waistcoat fashion axe chia unicorn
-              </h2>
+              <Link href='/posts/[slug]' as={`/posts/${post.slug}`}>
+                <a className="bloglink">
+                  <h2 className="text-2xl font-medium text-gray-900 title-font">
+                    {post.title}
+                  </h2>
+                </a>
+              </Link>
+              <span className="mt-1 text-gray-500 text-sm">{readingTime(post.html).text}</span>
             </div>
-            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <span className="font-semibold title-font text-gray-700">
-                CATEGORY
+            <div className="md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <span className="font-semibold title-font text-gray-500">
+                {post.tags[0].name.toUpperCase()}
               </span>
-              <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+              <span className="mt-1 text-gray-500 text-sm">{format(new Date(post.published_at), 'dd/MM/yyyy')}</span>
             </div>
           </div>
+          ))
+        }
+
           
         </div>
       </div>
     </section>
+    <style jsx>
+      {`
+        .bloglink:hover {
+          @apply no-underline;
+        }
+      `}
+    </style>
   </>
-);
+  )
+};
 
 export { BlogGallery };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,18 @@
-import { format } from 'date-fns'
 import Head from 'next/head'
 import { BlogGallery } from '../components/BlogGallery'
 import { Shell } from '../components/Shell'
 import styles from '../styles/Home.module.css'
 import { getPosts } from '../utils/getPosts'
-const readingTime = require('reading-time')
 
 export const getStaticProps = async ({ params }) => {
   const posts = await getPosts('title,slug,tag,html,published_at');
-  const titles = posts.map(post => post.title)
-  const rawReadingTimes = posts.map(post => readingTime(post.html))
-  const readingTimes = rawReadingTimes.map(readingTime => readingTime.text)
-  const slugs = posts.map(post => post.slug)
-  const tags = posts.map(post => post.tags[0].name)
-  const rawDates = posts.map(post => post.published_at)
-  const dates = rawDates.map(date => format(new Date(date), 'dd/MM/yyyy'))
+  
   return {
-    props: { titles, readingTimes, slugs, tags, dates }
+    props: { posts }
   }
 }
 
-const Home = ({ titles, readingTimes,slugs, tags, dates }) => (
+const Home = ({ posts }) => (
   <div className={styles.container}>
     <Head>
       <title>raulmar</title>
@@ -28,13 +20,8 @@ const Home = ({ titles, readingTimes,slugs, tags, dates }) => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <Shell location="home">
-      <BlogGallery/>  
+      <BlogGallery posts={posts}/>  
     </Shell>
-    <div className="text-red-500">{JSON.stringify(titles)}</div>
-    <div>{JSON.stringify(readingTimes)}</div>
-    <div className='text-red-500'>{JSON.stringify(slugs)}</div>
-    <div>{JSON.stringify(tags)}</div>
-    <div className='text-red-500'>{JSON.stringify(dates)}</div>
   </div>
 )
 
